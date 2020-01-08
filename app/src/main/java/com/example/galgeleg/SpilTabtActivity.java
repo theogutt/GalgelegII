1cf9449118178535c81861829150d40f7750b414
theogutt <theodorguttesen@gmail.com>
2020-01-08 20:23:47 +0100

    lyd virker

--- app/src/main/java/com/example/galgeleg/SpilTabtActivity.java
+++ app/src/main/java/com/example/galgeleg/SpilTabtActivity.java
@@ -3,7 +3,12 @@ package com.example.galgeleg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,17 +19,39 @@ public class SpilTabtActivity extends AppCompatActivity {
     Button spilIgen;
     TextView ordetVar;
     String ord;
-
+    boolean loaded = false;
+    private SoundPool soundPool;
+    int soundLose;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spil_tabt);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .build();
 
+            soundPool = new SoundPool.Builder()
+                    .setMaxStreams(6)
+                    .setAudioAttributes(audioAttributes)
+                    .build();
+        } else {
+            soundPool = new SoundPool(6, AudioManager.STREAM_MUSIC, 0);
+        }
+        soundLose = soundPool.load(this, R.raw.lose,1);
+        soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
+            public void onLoadComplete(SoundPool soundPool, int sampleId,int status) {
+                loaded = true;
+                playSoundLose();
+            }
+        });
         seHighscore = findViewById(R.id.button_se_highscore2);
         spilIgen = findViewById(R.id.button_spil_igen2);
         ordetVar = findViewById(R.id.textView_ordetVar);
         ord = getIntent().getStringExtra("rigtigtOrd");
         ordetVar.setText("Ordet var: "+ord);
+
 
         seHighscore.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,5 +73,17 @@ public class SpilTabtActivity extends AppCompatActivity {
     public void spilIgen(){
         Intent intent = new Intent(this, SpilActivity.class);
         startActivity(intent);
+    }
+    public void playSoundLose(){
+        if (loaded) {
+            Log.i("Sound","should play");
+            soundPool.play(soundLose, 1, 1, 0, 0, 1);
+        }
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        soundPool.release();
+        soundPool = null;
     }
 }
--- app/src/main/java/com/example/galgeleg/SpilVundetActivity.java
+++ app/src/main/java/com/example/galgeleg/SpilVundetActivity.java
@@ -3,7 +3,12 @@ package com.example.galgeleg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.widget.Button;
@@ -18,12 +23,34 @@ public class SpilVundetActivity extends AppCompatActivity {
     TextView antalForsøg;
     String forsøg;
     LottieAnimationView animation;
-
+    boolean loaded = false;
+    private SoundPool soundPool;
+    int soundWin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spil_vundet);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .build();
+
+            soundPool = new SoundPool.Builder()
+                    .setMaxStreams(6)
+                    .setAudioAttributes(audioAttributes)
+                    .build();
+        } else {
+            soundPool = new SoundPool(6, AudioManager.STREAM_MUSIC, 0);
+        }
+        soundWin = soundPool.load(this, R.raw.win,1);
+        soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
+            public void onLoadComplete(SoundPool soundPool, int sampleId,int status) {
+                loaded = true;
+                playSoundWin();
+            }
+        });
         animation = findViewById(R.id.animation);
         animation.playAnimation();
         seHighscore = findViewById(R.id.button_se_highscores);
@@ -52,5 +79,17 @@ public class SpilVundetActivity extends AppCompatActivity {
     public void spilIgen(){
         Intent intent = new Intent(this, SpilActivity.class);
         startActivity(intent);
+    }
+    public void playSoundWin(){
+        if (loaded) {
+            Log.i("Sound","should play");
+            soundPool.play(soundWin, 1, 1, 0, 0, 1);
+        }
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        soundPool.release();
+        soundPool = null;
     }
 }
